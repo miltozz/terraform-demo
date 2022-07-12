e3824383d1861bccb424737a486e23a89b274a5d
Miltos T <miltosdev@gmail.com>
2022-07-12 21:56:35 +0300

    updates

--- main.tf
+++ main.tf
@@ -11,25 +11,46 @@ provider "aws" {
   region = "eu-west-3"
 }
 
-variable vpc_cidr_block {}
-variable subnet_cidr_block {}
-variable avail_zone {}
-variable depl_env_prefix {}
+variable "vpc_cidr_block" {}
+variable "subnet_cidr_block" {}
+variable "avail_zone" {}
+variable "depl_env_prefix" {}
 
 resource "aws_vpc" "myapp-vpc" {
-    cidr_block = var.vpc_cidr_block
-    tags = {
-        Name = "${var.depl_env_prefix}-tf-myapp-vpc"
-    }
+  cidr_block = var.vpc_cidr_block
+  tags = {
+    Name = "${var.depl_env_prefix}-tf-myapp-vpc"
+  }
 }
 
 resource "aws_subnet" "myapp-subnet-1" {
-    vpc_id = aws_vpc.myapp-vpc.id
-    cidr_block = var.subnet_cidr_block
-    availability_zone = var.avail_zone
-    tags = {
-        Name = "${var.depl_env_prefix}-tf-myapp-subnet-1"
-    }
+  vpc_id            = aws_vpc.myapp-vpc.id
+  cidr_block        = var.subnet_cidr_block
+  availability_zone = var.avail_zone
+
+  tags = {
+    Name = "${var.depl_env_prefix}-tf-myapp-subnet-1"
+  }
 }
+
+resource "aws_internet_gateway" "myapp-igw" {
+  vpc_id = aws_vpc.myapp-vpc.id
+  tags = {
+    Name = "${var.depl_env_prefix}-tf-myapp-igw"
+  }
+}
+
+resource "aws_route_table" "myapp-route-table" {
+  vpc_id = aws_vpc.myapp-vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.myapp-igw.id
+  }
+  tags = {
+    Name = "${var.depl_env_prefix}-tf-myapp-rtb"
+  }
+}
+
 
 
