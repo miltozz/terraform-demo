aca9d4fa4af1912cd08dc64ae418b8a2682d5308
Miltos T <miltosdev@gmail.com>
2022-07-28 20:57:17 +0300

    deploy-3-ec2micros-learn-boto-status-check

--- main.tf
+++ main.tf
@@ -8,7 +8,7 @@ terraform {
 }
 
 provider "aws" {
-  region = "eu-west-3"
+  region = "eu-central-1"
 }
 
 
@@ -20,6 +20,22 @@ variable "depl_env_prefix" {}
 variable "my_ip" {}
 variable "instance_type" {}
 variable "public_key_location" {}
+
+data "aws_ami" "amazon-linux-latest" {
+  most_recent = true
+  owners      = ["137112412989"] # Amazon
+
+  filter {
+    name   = "name"
+    values = ["amzn2-ami-hvm*-x86_64-gp2"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+}
 
 resource "aws_vpc" "myapp-vpc" {
   cidr_block = var.vpc_cidr_block
@@ -38,43 +54,8 @@ resource "aws_subnet" "myapp-subnet-1" {
   }
 }
 
-resource "aws_internet_gateway" "myapp-igw" {
-  vpc_id = aws_vpc.myapp-vpc.id
-  tags = {
-    Name = "${var.depl_env_prefix}-tf-myapp-igw"
-  }
-}
-
-
-# Option 1: Create new aws_route_table, and then aws_route_table_association 
-# Option 2: Use the aws created default route table, as below (adoption) (advanced resource)
-
-resource "aws_default_route_table" "myapp-default-rtb" {
-  default_route_table_id = aws_vpc.myapp-vpc.default_route_table_id
-
-  route {
-    cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.myapp-igw.id
-  }
-
-  tags = {
-    Name = "${var.depl_env_prefix}-tf-myapp-default-rtb"
-  }
-}
-
-/* Option 1: Create security group
 resource "aws_security_group" "myapp-sg" {
-  name        = "${var.depl_env_prefix}-tf-myapp-sg"
-  description = "myapp security group"
-  vpc_id      = aws_vpc.myapp-vpc.id
-  ...
-  ingress
-  egress
-  ...
-*/
-
-# Option 2: Use the aws created, default security group (adoption) (advanced resource)
-resource "aws_default_security_group" "myapp-default-sg" {
+  name   = "myapp-sg"
   vpc_id = aws_vpc.myapp-vpc.id
 
   ingress {
@@ -103,24 +84,36 @@ resource "aws_default_security_group" "myapp-default-sg" {
   }
 
   tags = {
-    Name = "${var.depl_env_prefix}-myapp-use-default-sg"
+    Name = "${var.depl_env_prefix}-myapp-sg"
   }
 }
 
-data "aws_ami" "amazon-linux-latest" {
-  most_recent = true
+resource "aws_internet_gateway" "myapp-igw" {
+  vpc_id = aws_vpc.myapp-vpc.id
 
-  filter {
-    name   = "name"
-    values = ["amzn2-ami-hvm*-x86_64-gp2"]
+  tags = {
+    Name = "${var.depl_env_prefix}-tf-myapp-igw"
+  }
+}
+
+
+resource "aws_route_table" "myapp-rtb" {
+  vpc_id = aws_vpc.myapp-vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.myapp-igw.id
   }
 
-  filter {
-    name   = "virtualization-type"
-    values = ["hvm"]
+  tags = {
+    Name = "${var.depl_env_prefix}-tf-myapp-rtb"
   }
+}
 
-  owners = ["137112412989"] # Amazon
+# Associate subnet with Route Table
+resource "aws_route_table_association" "a-rtb-subnet" {
+  subnet_id      = aws_subnet.myapp-subnet-1.id
+  route_table_id = aws_route_table.myapp-rtb.id
 }
 
 //ssh key myst be present and created beforehand
@@ -132,25 +125,13 @@ resource "aws_key_pair" "myapp-ssh-key" {
 resource "aws_instance" "myapp-server" {
   ami                         = data.aws_ami.amazon-linux-latest.id
   instance_type               = var.instance_type
-  subnet_id                   = aws_subnet.myapp-subnet-1.id
-  vpc_security_group_ids      = [aws_default_security_group.myapp-default-sg.id]
-  availability_zone           = var.avail_zone
+  key_name                    = "myapp-server-key"
   associate_public_ip_address = true
-  key_name                    = aws_key_pair.myapp-ssh-key.key_name
+  subnet_id                   = aws_subnet.myapp-subnet-1.id
+  vpc_security_group_ids      = [aws_security_group.myapp-sg.id]
+  availability_zone           = var.avail_zone
   user_data_replace_on_change = true //forces instance recreation
 
-
-  /*
-  Note 1: user_data: 
-  user_data must be available by the cloud provider.
-  Terraform does not wait for execution or gives feeedback about success
-  or failure on these scripts. It just passes data on the cloud provider. 
-  Debug or reports are not available. Got to SSH to check if everything went OK
-
-  Note 2: user_data touches on configuration management by running shell scripts.
-  Terraform is better suited for infra provisioning. Ansible, Puppet or Chef are 
-  better choices for configuring stuff. 
-*/
   user_data = file("entry-script.sh") //if no file is used, <<EOF syntax needed
 
   tags = {
@@ -158,6 +139,39 @@ resource "aws_instance" "myapp-server" {
   }
 }
 
+resource "aws_instance" "myapp-server-2" {
+  ami                         = data.aws_ami.amazon-linux-latest.id
+  instance_type               = var.instance_type
+  key_name                    = "myapp-server-key"
+  associate_public_ip_address = true
+  subnet_id                   = aws_subnet.myapp-subnet-1.id
+  vpc_security_group_ids      = [aws_security_group.myapp-sg.id]
+  availability_zone           = var.avail_zone
+  user_data_replace_on_change = true //forces instance recreation
+
+  user_data = file("entry-script.sh") //if no file is used, <<EOF syntax needed
+
+  tags = {
+    Name = "${var.depl_env_prefix}-My App Server-2"
+  }
+}
+
+resource "aws_instance" "myapp-server-3" {
+  ami                         = data.aws_ami.amazon-linux-latest.id
+  instance_type               = var.instance_type
+  key_name                    = "myapp-server-key"
+  associate_public_ip_address = true
+  subnet_id                   = aws_subnet.myapp-subnet-1.id
+  vpc_security_group_ids      = [aws_security_group.myapp-sg.id]
+  availability_zone           = var.avail_zone
+  user_data_replace_on_change = true //forces instance recreation
+
+  user_data = file("entry-script.sh") //if no file is used, <<EOF syntax needed
+
+  tags = {
+    Name = "${var.depl_env_prefix}-My App Server-3"
+  }
+}
 output "data-AMI-id-found" {
   value = data.aws_ami.amazon-linux-latest.id
 }
