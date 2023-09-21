68a6b679c3e00c5c29dfceee44929ea93a139eb6
Miltos T <miltosdev@gmail.com>
2023-09-21 13:51:20 +0300

    trivial-updates

--- entry-script.sh
+++ entry-script.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
+echo "Start entry-script.sh"
 sudo yum update -y
 sudo yum install -y docker
 sudo systemctl start docker
 sudo usermod -aG docker ec2-user
 docker run -p 8080:80 nginx
+echo "End entry-script.sh"
